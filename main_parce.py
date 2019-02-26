1cbdfda187a49911d46154603d1b38acdf0243f1
v.kolomiiets <vetalboy.94@yandex.ru>
2019-02-26 17:46:02 +0300

    Первый парсинг и пуш на гит

--- main_parce.py
+++ main_parce.py
@@ -1,4 +1,5 @@
-#import scrapy
+from bs4 import BeautifulSoup
+from lxml import html
 
 import requests
 #user_id = str(12345)
@@ -11,3 +12,20 @@ r = requests.get(url, headers = headers)
 
 with open('test.html', 'wb') as output_file:
     output_file.write(r.text.encode('cp1251'))
+filename = 'test.html'
+
+def read_file(filename):
+    with open(filename) as input_file:
+        text = input_file.read()
+    return text
+
+def find_num(filename):
+    text = read_file(filename)
+    soup = BeautifulSoup(text, 'lxml')
+    #soup.find('div', {'class': 'profileFilmsList'})
+    #kurs = soup.find('div', {'class': 't18'}).find_all('b').text
+    print ([tag.text.strip() for tag in soup.find_all('div', {'class': 't18'})])
+    #return kurs
+    #print (kurs)
+
+find_num(filename)
